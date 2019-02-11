eed3d5afb0d1cff9f9ea1fc0ab9d4e327078bd70
Isuru Siriwardana <isuru.siriwardana@zenika.com>
2019-02-11 12:05:48 +0800

    Migrated to JUnit 5

--- src/test/java/person/service/PersonDetailsServiceImplTest.java
+++ src/test/java/person/service/PersonDetailsServiceImplTest.java
@@ -1,18 +1,17 @@
 package person.service;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import person.model.PersonDetails;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(properties = {"spring.profiles.active=test"})
+@SpringBootTest
+@ActiveProfiles(value = "test")
 public class PersonDetailsServiceImplTest {
 
 	@Autowired
@@ -24,12 +23,12 @@ public class PersonDetailsServiceImplTest {
 
 		personDetailService.insert(new PersonDetails("Hello", 25, "M", null, null));
 		List<PersonDetails> personDetails = personDetailService.loadAllPersonDetails();
-		Assert.assertEquals("Inserting new entry to person details table did not happen as expected", 5, personDetails.size());
+		Assertions.assertEquals(5, personDetails.size(), "Inserting new entry to person details table did not happen as expected");
 	}
 
 	@Test
 	public void testLoadAll() {
 		List<PersonDetails> personDetails = personDetailService.loadAllPersonDetails();
-		Assert.assertEquals("Number of person details received does not match the expected value", 4, personDetails.size());
+		Assertions.assertEquals(4, personDetails.size(), "Number of person details received does not match the expected value");
 	}
 }
